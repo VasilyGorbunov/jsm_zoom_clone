443e35119cfedae478fb1493d85c6ad3b432fe7e
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-04-02 20:08:40 +0300

    3:05:00

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,22 +1,25 @@
 import {useState} from "react";
 import {
     CallControls,
+    CallingState,
     CallParticipantsList,
     CallStatsButton,
     PaginatedGridLayout,
-    SpeakerLayout
+    SpeakerLayout,
+    useCallStateHooks
 } from "@stream-io/video-react-sdk";
 import {cn} from "@/lib/utils";
 import {
     DropdownMenu,
-    DropdownMenuContent, DropdownMenuItem,
-    DropdownMenuLabel,
+    DropdownMenuContent,
+    DropdownMenuItem,
     DropdownMenuSeparator,
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
 import {LayoutList, Users} from "lucide-react";
 import {useSearchParams} from "next/navigation";
 import EndCallButton from "@/components/EndCallButton";
+import Loader from "@/components/Loader";
 
 type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right'
 
@@ -25,6 +28,11 @@ const MeetingRoom = () => {
     const isPersonalRoom = !!searchParams.get('personal')
     const [layout, setLayout] = useState<CallLayoutType>('speaker-left')
     const [showParticipants, setShowParticipants] = useState(false)
+
+    const {useCallCallingState} = useCallStateHooks()
+    const callingState = useCallCallingState()
+
+    if (callingState !== CallingState.JOINED) return <Loader/>
 
     const CallLayout = () => {
         switch (layout) {
@@ -50,7 +58,7 @@ const MeetingRoom = () => {
                     <CallParticipantsList onClose={() => setShowParticipants(false)}/>
                 </div>
             </div>
-            <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+            <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
                 <CallControls/>
 
                 <DropdownMenu>
